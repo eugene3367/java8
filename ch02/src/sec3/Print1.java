5dbeaf8e22b3503e2aa5cb737b8945c179650b7f
eugene3367 <dbwls2518@naver.com>
2022-09-02 17:45:06 +0900

    add java print

---
+++ ch02/src/sec3/Print1.java
@@ -0,0 +1,18 @@
+package sec3;
+
+public class Print1 {
+
+	public static void main(String[] args) {
+		System.out.print("\na\n");	//ln 빠졌기때문에 줄바꿈 안됨 ->\n : new line
+		System.out.print("b\n");
+		System.out.print("c\n");
+		
+		System.out.println("\nd");
+		System.out.println("e");
+		System.out.println("f");
+		System.out.println("번호\t이름\t국어\t영어\t수학\t총점\\\""); 	//\t tab 추가, \와 "출력하고싶다면 역슬래쉬를 입력후 \,"
+		
+
+	}
+
+}
