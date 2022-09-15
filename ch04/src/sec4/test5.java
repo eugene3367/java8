e379b8b39d4f68633e5e2814a378f15b0d5cdebd
eugene3367 <dbwls2518@naver.com>
2022-09-15 17:22:34 +0900

    for, while

---
+++ ch04/src/sec4/test5.java
@@ -0,0 +1,20 @@
+package sec4;
+
+public class test5 {
+
+	public static void main(String[] args) {
+		/* 중첩 for문을 이용하여 방정식 4x+5y=60의 모든 해를 구해서 (x,y)형태로 출력해보세요.
+		 * 단, x와 y는 10이하의 자연수입니다.
+		 * (5,8)
+		 * (10,4)
+		 * 
+		 */
+
+		for(int x=1;x<=13;x++){
+			for(int y=1;y<=11;y++)
+				if(4*x+5*y==60)
+			System.out.println("("+x+", "+y+")");
+		}
+	}
+
+}
