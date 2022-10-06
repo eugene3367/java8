197a1ffef3a4dd1dbd432ae30c7c13e3340833bf
eugene3367 <dbwls2518@naver.com>
2022-10-06 18:26:13 +0900

    generic

--- ch13/src/sec1/ClassEx1.java
+++ ch13/src/sec1/ClassEx1.java
@@ -3,8 +3,44 @@ package sec1;
 public class ClassEx1 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
+}
+
+class AnimalList<T extends>
+
+
+
+class LandAnimal{
+	public void crying(){
+		System.out.println("육지동물");
+	}
+}
+
+class Cat extends LandAnimal{
+
+	@Override
+	public void crying() {
+		System.out.println("야옹야옹");
+	}
+	
+	
+}
+class Dog extends LandAnimal{
+
+	@Override
+	public void crying() {
+		System.out.println("멍멍");
+	}
+	
+}
+class Birds extends LandAnimal{
+
+	@Override
+	public void crying() {
+		System.out.println("짹쨱");
+	}
+	
 }
